dcf763cf92018d988536900aad8253be2ad4e4a9
Pius Muriithi <piusmuriithi1@gmail.com>
2022-05-12 22:09:46 +0300

    Using state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
